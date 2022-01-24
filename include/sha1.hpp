61107e019b098e687dae4f74909bd1af82665b96
Anjan Roy <anjanroy@yandex.com>
2022-01-24 03:48:06 +0000

    pad input byte array to so that message blocks can be parsed now

--- include/sha1.hpp
+++ include/sha1.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <CL/sycl.hpp>
+#include <cassert>
 
 namespace sha1 {
 
@@ -41,6 +42,55 @@ inline sycl::uint
 maj(sycl::uint x, sycl::uint y, sycl::uint z)
 {
   return (x & y) ^ (x & z) ^ (y & z);
+}
+
+// Given 40 -bytes input array, where two SHA1 digests are concatenated
+// produces output byte array of 64 -bytes, which is message block length of
+// SHA1, by padding input with 1 bit, required many zero bits and finally length
+// of input bit array ( = 320 -bits )
+//
+// In next step output byte array can be processed to have 16 SHA1 words ( each
+// word 32 -bit wide, obtained from four consecutive big-endian bytes )
+void
+pad_input_message(const sycl::uchar* __restrict in,
+                  size_t in_bit_len,
+                  sycl::uchar* const __restrict out,
+                  size_t out_bit_len)
+{
+  // input is always 40 -bytes i.e. two sha1 digests concatenated
+  assert(in_bit_len == 320);
+  // output is 64 -bytes i.e. a message block of sha1
+  assert(out_bit_len == 512);
+
+  // attempt to parallelize copying of input to output byte array
+  //
+  // copies first 40 -bytes as it's
+  //
+  // note, this loop will be partially unrolled
+#pragma unroll 8
+  for (size_t i = 0; i < (in_bit_len >> 3); i++) {
+    *(out + i) = *(in + i);
+  }
+
+  const sycl::uint offset = in_bit_len >> 3;
+
+  // then set 41 -st byte, as defined in sha1 specification
+  *(out + offset) = 0b10000000;
+
+  // now set next 21 -bytes to zero, it can be parallelized, due to
+  // absense of any loop carried dependencies
+  //
+  // note, this loop will be partially unrolled, though unroll factor is not
+  // power of 2 !
+#pragma unroll 7
+  for (size_t i = 1; i < 22; i++) {
+    *(out + offset + i) = 0;
+  }
+
+  // set last two bytes to length of input ( in terms of bits )
+  // which is 320 -bit
+  *(out + 62) = 0b00000001;
+  *(out + 63) = 0b01000000;
 }
 
 }
