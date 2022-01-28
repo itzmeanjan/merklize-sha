ba5a409936ec90e04922518fe35b398c3f3ce475
Anjan Roy <anjanroy@yandex.com>
2022-01-28 05:01:14 +0000

    execute all possible test cases

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# easy to use script for executing all possible test cases !
+
+make clean
+
+SHA=sha1     make; make clean
+SHA=sha2_224 make; make clean
+SHA=sha2_256 make; make clean
+SHA=sha2_384 make; make clean
+SHA=sha2_512 make; make clean
+  
