cbcbdbe05f90e99476d59b644e699724d02c0268
Anjan Roy <anjanroy@yandex.com>
2022-01-31 05:24:32 +0000

    keccak-p step mapping `ι`

--- include/sha3.hpp
+++ include/sha3.hpp
@@ -13,7 +13,7 @@ inline void
 {
   std::bitset<64> c[5];
 
-  // see step 1 of algorithm 1 in http://dx.doi.org/10.6028/NIST.FIPS.202
+  // see step 1 of algorithm 1
 #pragma unroll 5
   for (size_t x = 0; x < 5; x++) {
     c[x] = state[x][0] ^ state[x][1] ^ state[x][2] ^ state[x][3] ^ state[x][4];
@@ -21,7 +21,7 @@ inline void
 
   std::bitset<64> d[5];
 
-  // see step 2 of algorithm 1 in http://dx.doi.org/10.6028/NIST.FIPS.202
+  // see step 2 of algorithm 1
   for (size_t x = 0; x < 5; x++) {
 #pragma unroll 64
     for (size_t z = 0; z < 64; z++) {
@@ -29,7 +29,7 @@ inline void
     }
   }
 
-  // see step 3 of algorithm 1 in http://dx.doi.org/10.6028/NIST.FIPS.202
+  // see step 3 of algorithm 1
 #pragma unroll 5
   for (size_t y = 0; y < 5; y++) {
 #pragma unroll 5
@@ -48,23 +48,23 @@ inline void
 inline void
 ρ(const std::bitset<64>** state_in, std::bitset<64>** const state_out)
 {
-  // step 1 of algorithm 2 in http://dx.doi.org/10.6028/NIST.FIPS.202
+  // step 1 of algorithm 2
   state_out[0][0] = state_in[0][0];
 
-  // step 2 of algorithm 2 in http://dx.doi.org/10.6028/NIST.FIPS.202
+  // step 2 of algorithm 2
   size_t x = 1;
   size_t y = 0;
 
-  // step 3 of algorithm 2 in http://dx.doi.org/10.6028/NIST.FIPS.202
+  // step 3 of algorithm 2
   for (size_t t = 0; t < 24; t++) {
-    // step 3a of algorithm 2 in http://dx.doi.org/10.6028/NIST.FIPS.202
+    // step 3a of algorithm 2
     for (size_t z = 0; z < 64; z++) {
       size_t _z = 63 - (z - (((t + 1) * (t + 2)) / 2)) % 64;
 
       state_out[x][y][z] = state_in[x][y][_z];
     }
 
-    // step 3b of algorithm 2 in http://dx.doi.org/10.6028/NIST.FIPS.202
+    // step 3b of algorithm 2
     const size_t tmp = x;
     x = y;
     y = (2 * tmp + 3 * y) % 5;
@@ -80,7 +80,7 @@ inline void
 inline void
 π(const std::bitset<64>** state_in, std::bitset<64>** const state_out)
 {
-  // step 1 of algorithm 3 in http://dx.doi.org/10.6028/NIST.FIPS.202
+  // step 1 of algorithm 3
   for (size_t z = 0; z < 64; z++) {
     // a single slice
 #pragma unroll 5
@@ -103,7 +103,7 @@ inline void
 inline void
 χ(const std::bitset<64>** state_in, std::bitset<64>** const state_out)
 {
-  // step 1 of algorithm 4 in http://dx.doi.org/10.6028/NIST.FIPS.202
+  // step 1 of algorithm 4
   for (size_t x = 0; x < 5; x++) {
     for (size_t y = 0; y < 5; y++) {
       for (size_t z = 0; z < 64; z++) {
@@ -115,4 +115,48 @@ inline void
       }
     }
   }
+}
+
+// See algorithm 5 in section 3.2.5 of http://dx.doi.org/10.6028/NIST.FIPS.202
+inline bool
+rc(size_t t)
+{
+  if (t % 255 == 0) {
+    return 1;
+  }
+
+  std::bitset<9> r{ 0b010000000 };
+  for (size_t i = 1; i <= t % 255; i++) {
+    r[8] = r[8] ^ r[0];
+    r[4] = r[4] ^ r[0];
+    r[3] = r[3] ^ r[0];
+    r[2] = r[2] ^ r[0];
+
+    r[0] = 0;
+  }
+
+  return r[7];
+}
+
+// keccak-p[b, n_r] step mapping
+//
+// Input is 5 x 5 x 64 state array, along with round index ∈ [0, 24)
+// Output is modified state array
+//
+// See specification of `ι` step mapping function in section 3.2.5
+// of http://dx.doi.org/10.6028/NIST.FIPS.202
+inline void
+ι(std::bitset<64>** const state, size_t round_index)
+{
+  // step 2 of algorithm 6
+  std::bitset<64> RC;
+  RC.reset();
+
+  // step 3 of algorithm 6
+  for (size_t j = 0; j < 7; j++) {
+    RC[63 - ((1 << j) - 1)] = rc(j + 7 * round_index);
+  }
+
+  // step 4 of algorithm 6
+  state[0][0] ^= RC;
 }
